62245152de03256e42042328789ffbcb59667c96
J0K3R-Joel <kalkusch.joel@gmail.com>
2026-08-13 15:10:45 +0300

    changed: messageR.py got new methods to print messages in the console. background_timer.py got a update concerning the place_mode_widget function

--- src/background_timer.py
+++ src/background_timer.py
@@ -1,13 +1,42 @@
 import time
 import customtkinter
+import messageR
+import inspect
+
+msg = messageR.MessageR('Global')
+
 
 MODE_START_Y = 0.2
 MODE_Y_INCREASER = 0.075
+PLACEMENT_WIDGETS = {}
 
 
 def place_mode_widget(widget: customtkinter, placement: int) -> None:
-    if not isinstance(widget, customtkinter):
+    shove_widgets_back = False
 
+    if placement > len(PLACEMENT_WIDGETS):
+        placement = len(PLACEMENT_WIDGETS) + 1 if len(PLACEMENT_WIDGETS) > 0 else 0
+    if placement < 0:
+        placement = 0
+
+    for p in PLACEMENT_WIDGETS.keys():
+        if p < placement:
+            continue
+        elif p == placement and not shove_widgets_back:
+            shove_widgets_back = True
+            placement += 1
+            continue
+
+        if shove_widgets_back:
+            PLACEMENT_WIDGETS[p] =
+
+
+
+
+
+
+    y_cord = MODE_START_Y + MODE_Y_INCREASER * placement
+    widget.place(relx=0.5, rely=)
 
 
 timer_app = customtkinter.CTk()
@@ -41,4 +70,5 @@ timer_button_mode.place(relx=0.5, rely=0.2, anchor='center')
 timer_label_content = customtkinter.CTkLabel(content_site, text='Timer')
 timer_label_content.place(relx=0.5, rely=0.2, anchor='center')
 
+place_mode_widget(timer_label_content, 1)
 timer_app.mainloop()
--- src/messageR.py
+++ src/messageR.py
@@ -1,3 +1,28 @@
 class MessageR:
-    def __init__(self):
+    def __init__(self, name: str):
+        self.name = name
+        self.default_str = f'[{self.name}]: '
+
+    def __write__(self, *messages, status: str):
+        text = ''.join(messages)
+        final_message = f'> {status.upper()} - ' + self.default_str + text
+        print(final_message, flush=True)
+
+    def warning(self, *messages):
+        self.__write__(*messages, status='warning')
+
+    def error(self, *messages):
+        self.__write__(*messages, status='error')
+
+    def info(self, *messages):
+        self.__write__(*messages, status='info')
+
+    def bad(self, *messages):
+        self.__write__(*messages, status='bad')
+
+    def good(self, *messages):
+        self.__write__(*messages, status='good')
+
+
+
         
